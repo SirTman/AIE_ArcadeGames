5561add6c00cfcc115b38389cd20b30acf6a65ea
SirTman <theo.inc.au@gmail.com>
2017-03-07 16:28:39 +1030

    Intial install
    -add files

---
+++ ArcadeGames/ArcadeGames/ArcadeGames.cpp
@@ -0,0 +1,20 @@
+// ArcadeGames.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+
+int main()
+{
+	bool TicTacToe = true;
+	//while (TicTacToe)
+	//{
+		std::cout << "-|-|-\n-+---\n";
+		std::cout << "X|-|-\n-----\n";
+		std::cout << "-|-|O\n\n";
+	//}
+	
+	system("pause");
+	return 0;
+}
+
